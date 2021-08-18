f729c3377fea37c6a0544e4421f15e0e62d32088
cingarrido <cinthiagarrido24@gmail.com>
2021-08-17 21:58:13 -0300

    claseCuatro

---
+++ ClaseCuatro/act.js
@@ -0,0 +1,57 @@
+//primer ejercicio//
+let temperatura= 30
+let llueve= false
+let piso=5
+let hora= 16
+let horaApertura= 14
+let horaCierre=21
+let cantidadQueso=100
+let deuda= 250
+//Fin//
+//Segundo Ejercicio: Entrar a Playground//
+let computadoraEncendida=true
+let hayInternet=true
+const entrarApagina="www.playground.com.ar"
+let usuario= "grupoSeis"
+let contrasenia= 1234
+let entrarMateria= "Programacion Imperativa"
+let entrarModulo= "Modulo dos"
+console.log(computadoraEncendida)
+console.log(hayInternet)
+console.log(entrarApagina)
+console.log(usuario)
+console.log(contrasenia)
+console.log(entrarMateria)
+console.log(entrarModulo)
+//Fin segundo ejercicio)//
+console.log()
+//tercer ejercicio:cenar//
+let hayHambre=true
+let plataDelivery=false
+let cantHuevos=5
+let kilosPapas=2
+let aceite=null
+let sarten="tortilla"
+let minCoccion= 15 
+let cantidadPersonas= 3
+console.log(hayHambre)
+console.log(plataDelivery)
+console.log(cantHuevos)
+console.log(kilosPapas)
+console.log(aceite)
+console.log(sarten)
+console.log(minCoccion)
+console.log(cantidadPersonas)
+// actividad extra//
+let cajonRemeras= "pantalones"
+let cajonPantalones="remeras"
+let cajonVacio;
+cajonVacio=cajonRemeras
+cajonRemeras=cajonPantalones
+cajonPantalones=cajonVacio
+console.log(cajonRemeras)
+console.log(cajonPantalones)
+console.log(cajonVacio)
+
+
+
